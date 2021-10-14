63b0c8ff4e301d6ecbf23cbe8e7b56f39200629f
Perry Defayette <defayetteperry@gmail.com>
2021-10-14 16:04:03 -0400

    Clean up userflowitem.

--- src/components/UserFlowTests/index.jsx
+++ src/components/UserFlowTests/index.jsx
@@ -15,8 +15,6 @@ import UserFlowListItem from "./userflowlistitem";
 import "./css/userflow.css";
 import axios from "axios";
 import { AuthContext } from "../../App";
-// import axios from "axios";
-// import ApiListItem from "../EndpointTests/apilistitem";
 
 export default function UserFlowMenu() {
   // Toggle between dev & staging
@@ -39,18 +37,9 @@ export default function UserFlowMenu() {
     apis: PrescriptionFlow,
   });
 
-  useEffect(() => {
-    console.log(results);
-  }, [results]);
-
   const expandRefs = useRef([]);
   const setExpandCallback = useCallback((callback) => {
     expandRefs.current.push(callback);
-  }, []);
-
-  const testRefs = useRef([]);
-  const setTestCallback = useCallback((callback) => {
-    testRefs.current.push(callback);
   }, []);
 
   const handleExpand = () => {
@@ -63,20 +52,20 @@ export default function UserFlowMenu() {
     }
   };
 
-  const renderFlowItem = (api, i) => {
+  const renderFlowItem = (res, i) => {
     return (
       <UserFlowListItem
         key={i}
-        api={api}
+        result={res.result}
+        api={res.url}
+        body={res.body}
         expandCallback={setExpandCallback}
-        testCallback={setTestCallback}
-        server={server}
-        setResults={setResults}
       />
     );
   };
 
-  const sendReq = async (api, url) => {
+  const sendReq = async (api, url, displayURL) => {
+    console.log(api);
     axiosQueue({
       method: api.method,
       url: url,
@@ -108,7 +97,10 @@ export default function UserFlowMenu() {
       .then((res) => {
         // console.log(res);
 
-        setResults((oldResults) => [...oldResults, res]);
+        setResults((oldResults) => [
+          ...oldResults,
+          { result: res, url: displayURL },
+        ]);
         // setShowMenu(true);
         // setShowData(true);
         // setLoading(false);
@@ -119,9 +111,6 @@ export default function UserFlowMenu() {
   };
 
   const handleFlow = async (list) => {
-    const server = apiVersion[0];
-    // const list = PatientFlow;
-
     try {
       const postReq = await axios({
         method: "POST",
@@ -132,8 +121,11 @@ export default function UserFlowMenu() {
           Accept: "application/json",
         },
       });
-      console.log(postReq);
-      setResults((oldResults) => [...oldResults, postReq]);
+      console.log({ postReq, url: list.post.url });
+      setResults((oldResults) => [
+        ...oldResults,
+        { result: postReq, url: list.post.url, body: list.post.body },
+      ]);
 
       // If testing, for example, patients
       if (postReq.data.result.demographicNo) {
@@ -146,29 +138,30 @@ export default function UserFlowMenu() {
               postReq.data.result.demographicNo +
               api.suffix +
               server.suffix;
+
+            const displayURL =
+              api.url + postReq.data.result.demographicNo + api.suffix;
             console.log(url);
-            return await sendReq(api, url);
+            return await sendReq(api, url, displayURL);
           } else {
             const url = server.endpointURL + api.url + server.suffix;
             console.log(url);
-            return await sendReq(api, url);
+            const displayURL = api.url;
+            return await sendReq(api, url, displayURL);
           }
         });
       } else {
         list.apiList.map(async (api) => {
           return await sendReq(
             api,
-            server.endpointURL + api.url + server.suffix
+            server.endpointURL + api.url + server.suffix,
+            api.url
           );
         });
       }
     } catch (e) {
       console.error(e);
     }
-
-    // setResults(arr);
-    // console.log(arr);
-    // Promise.allSettled(arr);
   };
 
   return (
@@ -182,7 +175,7 @@ export default function UserFlowMenu() {
             onClick={() => {
               setServer(apiVersion[0]);
               setToggle(!toggle);
-              testRefs.current = [];
+              // testRefs.current = [];
             }}
             disabled={toggle}
           >
@@ -194,7 +187,7 @@ export default function UserFlowMenu() {
             onClick={() => {
               setServer(apiVersion[1]);
               setToggle(!toggle);
-              testRefs.current = [];
+              // testRefs.current = [];
             }}
             disabled={!toggle}
           >
@@ -211,9 +204,6 @@ export default function UserFlowMenu() {
               setExpanded(false);
               setStyles({ display: "none" });
               handleFlow(flow.apis);
-              // testRefs.current.forEach(async (test) => {
-              //   await test();
-              // });
             }}
           >
             Test
@@ -236,7 +226,7 @@ export default function UserFlowMenu() {
               {flow.flow !== "prescription" ? (
                 <button
                   onClick={() => {
-                    testRefs.current = [];
+                    // testRefs.current = [];
 
                     handleExpand();
                     setFlow({
@@ -252,7 +242,7 @@ export default function UserFlowMenu() {
               {flow.flow !== "patient" ? (
                 <button
                   onClick={() => {
-                    testRefs.current = [];
+                    // testRefs.current = [];
 
                     handleExpand();
                     setFlow({
@@ -264,7 +254,6 @@ export default function UserFlowMenu() {
                   patient
                 </button>
               ) : null}
-              {/* <button>other</button> */}
             </div>
           </div>
           <button
@@ -282,32 +271,7 @@ export default function UserFlowMenu() {
       </div>
       <hr />
 
-      {results
-        ? results.map((result, i) => {
-            // return <Item key={i} result={result} />;
-            console.log(result);
-            return (
-              <UserFlowListItem
-                key={i}
-                result={result}
-                expandCallback={setExpandCallback}
-              />
-            );
-            // return <p key={i}>a</p>;
-          })
-        : null}
-      {/* {flow.flow === "prescription" ? (
-        <div>
-          <UserFlowListItem
-            api={flow.apis.post}
-            expandCallback={setExpandCallback}
-            testCallback={setTestCallback}
-            server={server}
-          />
-          {flow.apis.apiList.map(renderFlowItem)}
-        </div>
-      ) : null}
-      {flow.flow === "patient" ? flow.apis.apiList.map(renderFlowItem) : null} */}
+      {results ? results.map(renderFlowItem) : null}
     </div>
   );
 }
--- src/components/UserFlowTests/userflowlistitem.jsx
+++ src/components/UserFlowTests/userflowlistitem.jsx
@@ -1,100 +1,19 @@
 import { useCallback, useEffect, useState } from "react";
 import JSONPretty from "react-json-pretty";
-import Loader from "react-loader-spinner";
-// import { AuthContext } from "../../App";
-import axiosQueue from "../../helpers/axios";
 import StatusBox from "../statusbox";
 
 export default function UserFlowListItem(props) {
-  const { expandCallback, result } = props;
+  const { expandCallback, result, api, body } = props;
   const [showMenu, setShowMenu] = useState(false);
   const [showData, setShowData] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState({});
-
-  // const [id, setId] = useState(0);
-
-  // const { state } = useContext(AuthContext);
-  // const stateRef = useRef(state);
-
-  // useEffect(() => {
-  //   stateRef.current = state;
-  // }, [state]);
-
-  // useEffect(() => {
-  //   console.log("id changed", id);
-  // }, [id]);
-
-  // useEffect(() => {
-  //   return () => {
-  //     console.log("flowlistitem unmount");
-  //     // setResponse({});
-  //   };
-  // });
-  // useEffect(() => {
-  //   console.log("test callback");
-  //   setResponse({});
-  // }, [testCallback]);
 
   const expandContract = useCallback((isExpanded) => {
     isExpanded ? setShowMenu(false) : setShowMenu(true);
   }, []);
 
-  // const queryAPI = useCallback(() => {
-  //   // setResponse({});
-  //   setLoading(true);
-  //   // console.log(idNo);
-
-  //   const url = server.endpointURL + api.url + server.suffix;
-  //   if (api.id) {
-  //     console.log(api.id);
-  //   } else {
-  //     console.log("no api id", api.url + server.suffix);
-  //   }
-
-  //   axiosQueue({
-  //     method: api.method,
-  //     url: url,
-  //     data: api.body,
-  //     headers: {
-  //       Authorization: `Bearer ${stateRef.current.dev.token}`,
-  //       Accept: "application/json",
-  //     },
-  //   })
-  //     .then((res) => {
-  //       console.log(`Success calling ${api.url}`);
-  //       console.log(res.data);
-
-  //       return res;
-  //     })
-  //     .catch((err) => {
-  //       console.log(`Failed calling ${api.url}`);
-  //       if (err.response) {
-  //         console.log(err.response);
-  //       } else if (err.request) {
-  //         console.log(err.request);
-  //       } else {
-  //         console.log("Error", err.message);
-  //       }
-  //       console.log(err.config);
-
-  //       return err.response;
-  //     })
-  //     .then((res) => {
-  //       setResponse(res);
-  //       setShowMenu(true);
-  //       setShowData(true);
-  //       setLoading(false);
-  //       return new Promise((resolve) => {
-  //         resolve();
-  //       });
-  //     });
-  // }, [api, server]);
-
   useEffect(() => {
-    // Always register callbacks
+    // Register expand callback
     expandCallback(expandContract);
-    // testCallback(queryAPI);
   }, [expandCallback, expandContract]);
 
   return (
@@ -108,24 +27,13 @@ export default function UserFlowListItem(props) {
         <div className={"button-contents"}>
           <div className={"flex-left"}>
             <h2>
-              <b>({result.config.method})</b> {result.config.url}
-              {/* {api.suffix} */}
+              <b>({result.config.method})</b> {api}
             </h2>
           </div>
 
-          {loading ? (
-            <Loader
-              className="loading-results"
-              type="Bars"
-              color="rgb(0, 0, 0)"
-              height={15}
-              width={15}
-            />
-          ) : (
-            <div className="pass-fail-container">
-              <StatusBox response={result} />
-            </div>
-          )}
+          <div className="pass-fail-container">
+            <StatusBox response={result} />
+          </div>
         </div>
       </button>
 
@@ -136,14 +44,14 @@ export default function UserFlowListItem(props) {
               <b>URL: </b>
               {result.config.url}
             </p>
-            {/* {api.body ? (
+            {body ? (
               <div>
                 <p>
                   <b>Body:</b>
                 </p>
-                <JSONPretty id="json-pretty" data={api.body}></JSONPretty>
+                <JSONPretty id="json-pretty" data={body}></JSONPretty>
               </div>
-            ) : null} */}
+            ) : null}
 
             <p>
               <b>Data:</b>
